72bb7041c839feb76841ae6db355e23c6b173c99
jatin7gupta <jatin13csu055@ncuindia.edu>
2017-10-28 13:34:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
